264039498dd4567bff57c65b44920582b9658328
0-Mayday-0 <mateogreslebin@gmail.com>
2026-01-12 18:48:06 -0300

    Working async fetch
    Working async fetch and cache saving

--- emote_handler.py
+++ emote_handler.py
@@ -1,12 +1,16 @@
 import re
 from custom_error import InvalidEmote
-from requests import get
+from requests import get, Response
 import strings as strs
 from PIL import Image
+from dotenv import load_dotenv
 import io
+import os
+from asyncio import to_thread, run, create_task, Task
 
 class Emote:
     def __init__(self, url: str) -> None:
+        load_dotenv(strs.Handler.Internal.DOTENV_NAME)
         try:
             self._protocol: str = re.search(r'(https://)', url).group()
             self._subdomain: str = re.search(r'(cdn\.)', url).group()
@@ -35,16 +39,26 @@ class Emote:
         return (f'{self._protocol}{self._subdomain}{self._root_domain}{self._top_level_domain}{self._slug}'
                 f'{self._emote_ID}{self._extension}{self._config}')
 
-    def fetch_emote_img(self) -> Image:
-        return Image.open(io.BytesIO(get(self._assemble_url()).content))
+    async def fetch_emote_img(self) -> Image:
+        img_data: Response = await to_thread(get, self._assemble_url())
+        return Image.open(io.BytesIO(img_data.content))
+
+    async def save_to_cache(self) -> None:
+        emote_fetch_task: Task[Image] = create_task(self.fetch_emote_img())
+
+        emote_fetch: Image = await emote_fetch_task
+
+        emote_fetch.save(f'{os.getenv(strs.Handler.Internal.IMG_CACHE_NAME)}/{self._emote_ID}{self._extension.removesuffix('?')}')
 
     def __bytes__(self) -> bytes:
         return b''.join([bytes(i.encode('UTF-8')) for i in self._get_privates()])
 
-def main():
+async def main():
     test_emote: Emote = Emote("https://cdn.discordapp.com/emojis/1110989171809583156.gif?v=1&size=48&quality=lossless")
 
-    
+    save_to_cache_task: Task[None] = create_task(test_emote.save_to_cache())
+
+    await save_to_cache_task
 
 if __name__ == "__main__":
-    main()
+    run(main())
--- strings.py
+++ strings.py
@@ -5,6 +5,8 @@ class Handler:
 
     @dataclass(frozen=True)
     class Internal:
+        DOTENV_NAME: str = 'paths.env'
+        IMG_CACHE_NAME: str = 'IMAGE_CACHE'
         IMAGE_MODE: str = 'RGB'
 
     @dataclass(frozen=True)
