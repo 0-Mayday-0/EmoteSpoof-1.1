5cc70fc6fc0d8e6f91bcfb130ab59f17b20207d3
0-Mayday-0 <mateogreslebin@gmail.com>
2026-01-14 13:50:07 -0300

    Added necessary functions for DB handling

--- db.py
+++ db.py
@@ -5,33 +5,50 @@ import strings as strs
 from emote_handler import Emote, InvalidEmote
 
 class DbHandler:
-    def __init__(self):
+    def __init__(self, max_emotes: int):
         load_dotenv(strs.Handler.Internal.DOTENV_NAME)
         database_path: str = os.getenv(strs.Handler.Internal.EMOTE_DB_NAME)
         self._emote_db: tdb.TinyDB = tdb.TinyDB(database_path)
         self._query_object: tdb.Query = tdb.Query()
+        self.max_emotes_ppage: int = max_emotes
+        self.current_page: int = 1
 
     def add_emote(self, url) -> str:
         try:
             emote: Emote = Emote(url)
-            emote_id: str = emote.get_id()
+            emote_id: str = emote.get_id().replace('.', '')
             sanitized_url: str = emote.get_url()
 
-            if len(self._emote_db.search(self._query_object[emote_id] == sanitized_url)) > 0:
-                return strs.Handler.External.ALREADY_EXISTS.format(id=emote_id.replace('.', ''))
+            if len(self._emote_db.search(self._query_object[strs.Handler.Internal.EMOTE_ID] == emote_id)) > 0:
+                return strs.Handler.External.ALREADY_EXISTS.format(id=emote_id)
 
             else:
-                self._emote_db.insert({emote_id: sanitized_url})
-                return strs.Handler.External.ADDED_SUCCESS.format(id=emote_id.replace('.', ''))
+                self._emote_db.insert({strs.Handler.Internal.EMOTE_ID: emote_id,
+                                       strs.Handler.Internal.EMOTE_URL: sanitized_url})
+                return strs.Handler.External.ADDED_SUCCESS.format(id=emote_id)
 
         except InvalidEmote as e:
             return str(e)
 
+    def remove_emote(self, emote_id: str) -> str:
+        self._emote_db.remove(self._query_object[strs.Handler.Internal.EMOTE_ID] == emote_id)
+        return strs.Handler.External.REMOVED_SUCCESS.format(id=emote_id)
+
+    def emotes_pages(self) -> list[Emote]:
+        return [Emote(emote[strs.Handler.Internal.EMOTE_URL]) for emote in self._emote_db.all()[:self.max_emotes_ppage*self.current_page]]
+
+    def next_page(self) -> None:
+        self.current_page += 1
+
+    def previous_page(self) -> None:
+        self.current_page -= 1
 
 def main() -> None:
-    db: DbHandler = DbHandler()
+    db: DbHandler = DbHandler(10)
 
-    print(db.add_emote("https://cdn.discordapp.com/emojis/864309722199228436.png?v=1&size=48&quality=lossless"))
+    print(db.add_emote("https://cdn.discordapp.com/emojis/1119740756505145485.gif?v=1&size=48&quality=lossless"))
+
+    print(db.emotes_pages())
 
 if __name__ == '__main__':
     main()
--- strings.py
+++ strings.py
@@ -9,9 +9,12 @@ class Handler:
         IMG_CACHE_NAME: str = 'IMAGE_CACHE'
         EMOTE_DB_NAME: str = 'EMOTE_DB'
         IMAGE_MODE: str = 'RGB'
+        EMOTE_ID: str = 'emote_id'
+        EMOTE_URL: str = 'emote_url'
 
     @dataclass(frozen=True)
     class External:
         INVALID_URL: str = "The emote URL is invalid."
         ADDED_SUCCESS: str = "The emote with id {id} was added successfully."
         ALREADY_EXISTS: str = "The emote with id {id} already exists."
+        REMOVED_SUCCESS: str = "The emote with id {id} was removed successfully."
